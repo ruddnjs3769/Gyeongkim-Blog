ac299b586c52565dcaafd65b0b07c9d6ac1b6ad7
KimGyeongwon <ruddnjs6651@gmail.com>
2024-04-16 16:37:50 +0900

    settings: path alias 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import path from "path";
 
-// https://vitejs.dev/config/
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
